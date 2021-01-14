2036ee16d0f4974f1d870b3c148fd40438ca8428
CosmoStas <stasbkj@gmail.com>
2021-01-14 19:55:30 +0300

    Added Homework repo,  HomeworkDto

---
+++ EducationSystem/EducationSystem.Data/HomeworkRepository.cs
@@ -0,0 +1,58 @@
+﻿using EducationSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EducationSystem.Data
+{
+    public class HomeworkRepository
+    {
+        private string _connectionString = "Data Source=80.78.240.16;Initial Catalog=DevEdu;Persist Security Info=True;User ID=student;Password=qwe!23";
+        public HomeworkRepository()
+        {
+
+        }
+
+        public List<HomeworkDto> GetHomeworks()
+        {
+            SqlConnection sqlConnection = new SqlConnection(_connectionString);
+
+            sqlConnection.Open();
+
+            SqlCommand sqlCommand = new SqlCommand
+            {
+                CommandText = "dbo.Homework_SelectAll",
+                Connection = sqlConnection,
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+
+            var reader = sqlCommand.ExecuteReader();
+
+            List<HomeworkDto> result = new List<HomeworkDto>();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    result.Add(new HomeworkDto
+                    {
+                        Id = reader.GetInt32(0),
+                        Description = reader.GetString(1),
+                        StartDate = reader.GetDateTime(2),
+                        DeadlineDate = reader.GetDateTime(3),
+                        GroupID = reader.GetInt32(4),
+                        IsOptional = reader.GetBoolean(5),
+                        IsDeleted = reader.GetBoolean(6)
+                    });
+                }
+            }
+
+            reader.Close();
+
+            sqlConnection.Close();
+
+            return result;
+        }
+    }
+}
--- EducationSystem/EducationSystem/Controllers/WeatherForecastController.cs
+++ EducationSystem/EducationSystem/Controllers/WeatherForecastController.cs
@@ -27,8 +27,8 @@ namespace EducationSystem.Controllers
         [HttpGet]
         public dynamic Get()
         {
-            var repo = new CourseRepository();
-            return repo.GetCourses();
+            var repo = new HomeworkRepository();
+            return repo.GetHomeworks();
         }
     }
 }
