626b791339532bcabd9016dbd83d930dba792ebb
Чагай Сергей <frei.mizantrop@gmail.com>
2021-01-19 17:19:49 +0300

    Data transfer object for HomeworkAttempt_Attachment table

---
+++ EducationSystem/EducationSystem.Data/Models/HomeworkAttempt_AttachmentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationSystem.Data.Models
+{
+    class HomeworkAttempt_AttachmentDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int HomeworkId { get; set; }
+        public int AttachmentId { get; set; }
+        public int AttachmentTypeId { get; set; }
+    }
+}
