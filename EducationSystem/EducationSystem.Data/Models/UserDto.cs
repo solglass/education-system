c221f0cae39c6fb3115527acc0b696cb9df7c122
Olga <zemtsovskaya.ov@gmail.com>
2021-01-19 18:25:17 +0300

    Add Dapper methods for User, UserRole, Role

---
+++ EducationSystem/EducationSystem.Data/Models/UserDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationSystem.Data.Models
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string UserPic { get; set; }
+        public string Email { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
