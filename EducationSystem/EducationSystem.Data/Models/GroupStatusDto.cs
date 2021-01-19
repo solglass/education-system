67990fcb2174c1c6881ee0fcd313dda605b04e20
Galina <titengal@gmail.com>
2021-01-20 01:06:05 +0300

    Add Payment Dto and GroupStatus Dto,add dbo.GroupStatus_Update

---
+++ EducationSystem/EducationSystem.Data/Models/GroupStatusDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationSystem.Data.Models
+{
+    public class GroupStatusDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
