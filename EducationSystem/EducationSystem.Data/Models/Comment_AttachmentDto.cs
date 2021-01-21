dabdfeec2505179ebaf5db329ef2a8249649613f
Georgy <solglass@yandex.ru>
2021-01-21 16:45:57 +0300

    comment attachment dto push

--- EducationSystem/EducationSystem.Data/Models/Comment_AttachmentDto.cs
+++ EducationSystem/EducationSystem.Data/Models/Comment_AttachmentDto.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace EducationSystem.Data.Models
-{
+{ 
     public class Comment_AttachmentDto
     {
         public int Id { get; set; }
