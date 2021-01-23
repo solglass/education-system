b2004faa25580a5e86e9f3a7d4dee28d5fe4fb7e
CosmoStas <stasbkj@gmail.com>
2021-01-23 21:12:43 +0300

    Changes Dto

---
+++ EducationSystem/EducationSystem/Controllers/CourseController.cs
@@ -0,0 +1,40 @@
+﻿using EducationSystem.Controllers;
+using EducationSystem.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationSystem.API.Controllers
+{
+    // https://localhost:50221/api/user/
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private CourseRepository _repo;
+
+        public CourseController()
+        {
+            _repo = new CourseRepository();
+        }
+
+        [HttpGet("Course")]
+        public ActionResult GetCourse()
+        {
+            var courses = _repo.GetCourses();
+            return Ok(courses);
+        }
+
+        [HttpPost("Course/{name}/{description}/{duration}")]
+        public ActionResult AddCourse(string name, string description, int duration)
+        {
+            _repo.AddCourse(name, description, duration);
+            return Ok("пользователь зарегистрирован");
+        }
+
+    }
+}
