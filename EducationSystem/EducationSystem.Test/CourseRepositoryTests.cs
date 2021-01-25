ada6f1006d57a804012bf07cd1772ffb118226fc
Zhanna <yellowcucumber27@gmail.com>
2021-01-25 23:16:49 +0300

    Prepare for course methods testing

--- EducationSystem/EducationSystem.Test/CourseRepositoryTests.cs
+++ EducationSystem/EducationSystem.Test/CourseRepositoryTests.cs
@@ -10,37 +10,71 @@ namespace EducationSystem.Test
 {
     public class CourseRepositoryTest
     {
+        private int _courseId;
+        private List<int> _groupIdArray;
+        private List<int> _themeIdArray;
 
 
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(89)]
-        public void DeleteCourseTest(int id)
+
+
+        public List<GroupDto>  GetGroupMock(int n)
         {
-            CourseRepository courseRepository = new CourseRepository();
-
-
-            var expected = courseRepository.GetCourses();
-
-
-            var deleted = expected.Where(x => x.Id == id).ToList();
-            
-
-            expected.RemoveAll(x => x.Id == id);
-
-
-
-            courseRepository.DeleteCourse(id);
-
-            var actual = courseRepository.GetCourses();
-
-            for (int i = 0; i < deleted.Count; ++i)
+            List<GroupDto> groups = new List<GroupDto>() ;
+            switch (n)
             {
-              //  courseRepository.AddCourse(deleted[i].Name, deleted[i].Description, deleted[i].Duration);
+                case 1:
+                    return groups;
+                case 2:
+                    groups.Add(new GroupDto { StartDate = new System.DateTime(2020, 10, 12) });
+                    return groups;
+                case 3:
+                    groups.Add(new GroupDto { StartDate = new System.DateTime(2020, 10, 12) });
+                    groups.Add(new GroupDto { StartDate = new System.DateTime(2021, 10, 12) });
+                    return groups;
+                default:
+                    return groups;
             }
-
-            Assert.AreEqual(expected,actual);
         }
+        public List<ThemeDto> GetThemeMock(int n)
+        {
+            List<ThemeDto> themes = new List<ThemeDto>();
+            switch (n)
+            {
+                case 1:
+                    return themes;
+                case 2:
+                    themes.Add(new ThemeDto { Name="Test theme 1"});
+                    return themes;
+                case 3:
+                    themes.Add(new ThemeDto { Name = "Test theme 2" });
+                    themes.Add(new ThemeDto { Name = "Test theme 3" });
+                    return themes;
+                default:
+                    return themes;
+            }
+        }
+
+        public CourseDto GetCourseMock(int n)
+        {
+            CourseDto course = new CourseDto();
+            switch (n)
+            {
+                case 1:
+                    course = new CourseDto() { Name = "TestCourseCase 1", Description = "Test case 1", Duration = 1 };
+                    return course;
+                case 2:
+                    course = new CourseDto() { Name = "TestCourseCase 2", Description = "Test case 2", Duration = 2 };
+                    return course;
+                case 3:
+                    course = new CourseDto() { Name = "TestCourseCase 3", Description = "Test case 3", Duration = 3 };
+                    return course;
+                default:
+                    return course;
+            }
+        }
+
+
+
 
 
     }
