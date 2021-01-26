1a8f906ae7bad5d4260ea7d025dc307835c4dd56
Zhanna <yellowcucumber27@gmail.com>
2021-01-26 19:06:35 +0300

    fix parameters

--- EducationSystem/EducationSystem.Data/CourseRepository.cs
+++ EducationSystem/EducationSystem.Data/CourseRepository.cs
@@ -93,7 +93,7 @@ namespace EducationSystem.Data
         }
 
         // should return id of inserted entity, use 'QuerySingle' method
-        public int AddCourse(/*string name, string description, int duration*/CourseDto course)
+        public int AddCourse(CourseDto course)
         {
             var result = _connection
                 .QuerySingle<int>("dbo.Course_Add",
@@ -108,7 +108,7 @@ namespace EducationSystem.Data
         }
 
         // should return affected rows' count, use 'Execute' method
-        public int UpdateCourse(/*int id, string name, string description, int duration, bool isDeleted*/ CourseDto course)
+        public int UpdateCourse( CourseDto course)
         {
             var result = _connection
                 .Execute("dbo.Course_Update",
