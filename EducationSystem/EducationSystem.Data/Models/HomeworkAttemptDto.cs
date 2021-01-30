312e145eea64a1cda401e8f31d88bfb97e597ebf
Ira <brainforfreedom@gmail.com>
2021-01-30 21:36:13 +0300

    Update HWAttempt dapper

--- EducationSystem/EducationSystem.Data/Models/HomeworkAttemptDto.cs
+++ EducationSystem/EducationSystem.Data/Models/HomeworkAttemptDto.cs
@@ -12,9 +12,7 @@ namespace EducationSystem.Data.Models
         public int StatusId { get; set; }
         public bool IsDeleted { get; set; }
         public UserDto Author { get; set; }
-        public HomeworkDto Homework { get;set;}
         public HomeworkAttemptStatusDto HomeworkAttemptStatus { get; set; }
-        public List<AttachmentDto> Attachments { get; set; } // TODO: Check necessity
         public List<CommentDto> Comments { get; set; }
         
     }
