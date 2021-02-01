909cd2cb42dd1effd0ff9f1ab48c79e1c7008648
VitaxaOv <vit2013aly@mail.ru>
2021-02-01 15:13:52 +0300

    Add TagOutputModel

--- EducationSystem/EducationSystem/Mappers/TagMapper.cs
+++ EducationSystem/EducationSystem/Mappers/TagMapper.cs
@@ -1,5 +1,10 @@
 using EducationSystem.API.Models;
+using EducationSystem.API.Models.OutputModels;
 using EducationSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace EducationSystem.API.Mappers
 {
@@ -11,6 +16,45 @@ namespace EducationSystem.API.Mappers
                 Id = inputModel.Id,
                 Name = inputModel.Name
             };
+        }
+        public List<TagDto> ToDtos(List<TagInputModel> inputModels)
+        {
+            List<TagDto> result = new List<TagDto>();
+            if (inputModels == null || inputModels.Count == 0)
+            {
+                throw new Exception("Tags не найдены!");
+            }
+            foreach (var model in inputModels)
+            {
+                result.Add(ToDto(model));
+            }
+
+            return result;
+        }
+        public TagOutputModel FromDto(TagDto tagDto)
+        {
+            if (tagDto == null)
+            {
+                throw new Exception("Tag не найден!");
+            }
+            return new TagOutputModel()
+            {
+                Id = tagDto.Id,
+                Name = tagDto.Name
+            };
+        }
+        public List<TagOutputModel> FromDtos(List<TagDto> tagDtos)
+        {
+            List<TagOutputModel> models = new List<TagOutputModel>();
+            if (tagDtos == null || tagDtos.Count == 0)
+            {
+                throw new Exception("Tags не найдены!");
+            }
+            foreach (var tag in tagDtos)
+            {
+                models.Add(FromDto(tag));
+            }
+            return models;
         }
     }
 }
