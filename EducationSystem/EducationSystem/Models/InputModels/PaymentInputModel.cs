985a65c54710a58c1a238a777d0bac2d6397ab6c
Galina <titengal@gmail.com>
2021-02-02 17:01:57 +0300

    Added PaymentInputModel

---
+++ EducationSystem/EducationSystem/Models/InputModels/PaymentInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace EducationSystem.API.Models
+
+{
+    public class PaymentInputModel
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string Date { get; set; }
+        public string Period { get; set; }
+        public List <UserInputModel> Ids { get; set; }
+    
+    }
+}
