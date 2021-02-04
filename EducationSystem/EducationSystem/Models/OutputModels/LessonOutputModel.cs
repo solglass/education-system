1dab27c50dbc3d0e67084e7f4ef8aec3772d2f80
Sergey <Scorpion5149678@gmail.com>
2021-02-04 13:28:25 +0300

    Lesson InputModel, outputModel, Mapper

--- EducationSystem/EducationSystem/Models/OutputModels/LessonOutputModel.cs
+++ EducationSystem/EducationSystem/Models/OutputModels/LessonOutputModel.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EducationSystem.API.Models.InputModels
+namespace EducationSystem.API.Models.OutputModels
 {
-    public class LessonInputModel
+    public class LessonOutputModel
     {
         public int ID { get; set; }
         public int GroupID { get; set; }
         public string Comment { get; set; }
         public string LessonDate { get; set; }
-        public List<ThemeInputModel> Themes { get; set; }
+        public List<ThemeOutputModel> Themes { get; set; }
     }
 }
