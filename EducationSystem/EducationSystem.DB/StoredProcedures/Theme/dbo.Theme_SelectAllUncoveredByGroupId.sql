96bd81d0d89e85975a47857f19f02953b965437e
Zhanna <yellowcucumber27@gmail.com>
2021-02-04 23:12:11 +0300

    Add sql scripts for the first task

---
+++ EducationSystem/EducationSystem.DB/StoredProcedures/Theme/dbo.Theme_SelectAllUncoveredByGroupId.sql
@@ -0,0 +1,19 @@
+﻿create proc [dbo].[Theme_SelectAllUncoveredByGroupId]
+(@groupId int) as
+begin
+	select
+		g.Id,
+		g.CourseID,
+		g.StartDate,
+		t.Id,
+		t.Name
+	from dbo.[Group] g 
+		inner join dbo.[Course_Theme] ct on ct.CourseID=g.CourseID
+		left join dbo.[Theme] t on t.Id=ct.ThemeID 
+	WHERE g.Id=@groupId and t.Id NOT IN (SELECT t.Id 
+							from [dbo].[Lesson] l 
+								inner join dbo.Lesson_Theme lt on lt.LessonID=l.Id
+								inner join dbo.Theme t on t.Id=lt.ThemeID
+							where l.GroupID=@groupId and l.Date<(SELECT SYSDATETIME())) 
+			 
+end
