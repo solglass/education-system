c8642c147763d6dae28ab7906ea11dd72cce9ffd
AleksandrM178 <myshalow_aleksandar@mail.ru>
2021-02-06 01:58:48 +0300

    Add auth

---
+++ EducationSystem/EducationSystem.Core/Authentication/AuthOptions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace EducationSystem.Core.Authentication
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "EducationSystem.Api"; // издатель токена
+        public const string AUDIENCE = "DevEducation"; // потребитель токена
+        const string KEY = "djwaoijdajwdiawjdiawjdwaidjawodjawdjawodjoa*%#$$@efwsefw";   // ключ для шифрации
+        public const int LIFETIME = 1440; // время жизни токена - 1 минута
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
