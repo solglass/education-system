40380733d6d89978a1589c4aa7d4c6bd61b6e722
AleksandrM178 <myshalow_aleksandar@mail.ru>
2021-02-06 02:12:17 +0300

    Some fixe

--- EducationSystem/EducationSystem/Models/InputModels/UserInputModel.cs
+++ EducationSystem/EducationSystem/Models/InputModels/UserInputModel.cs
@@ -1,8 +1,7 @@
 ﻿using EducationSystem.Data.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace EducationSystem.API.Models.InputModels
 {
