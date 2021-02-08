bc7da8c228494ed36f616f3c58cf7e1813ad8111
Olga <zemtsovskaya.ov@gmail.com>
2021-02-08 05:58:01 +0300

    Add UserService

--- EducationSystem/EducationSystem.Business/UserService.cs
+++ EducationSystem/EducationSystem.Business/UserService.cs
@@ -8,18 +8,35 @@ namespace EducationSystem.Business
 {
     public class UserService
     {
-        private UserRepository _repo;
+        private UserRepository _userRepository;
         public UserService()
         {
-            _repo = new UserRepository();
+            _userRepository = new UserRepository();
         }
         public List<UserDto> GetUsers()
         {
-            return _repo.GetUsers();
+            return _userRepository.GetUsers();
         }
         public UserDto GetUserById(int id)
         {
-            return _repo.GetUserById(id);
+            return _userRepository.GetUserById(id);
         }
+        public int UpdateUser (UserDto userDto) 
+        { 
+            return _userRepository.UpdateUser(userDto); 
+        }
+        public int AddUser(UserDto userDto) 
+        { 
+            return _userRepository.AddUser(userDto); 
+        }
+        public int DeleteUser(int id)
+        {
+            return _userRepository.DeleteUser(id);
+        }
+        //public string ChangePassword(string login, string password)
+        //{
+        //    return _userRepository.CheckUser ();
+        //}
+        // TODO: Role 
     }
 }
