3b2bd3e610ee60c31ef7c2c99fcd31c2827a2dc6
ProgWidness <ProgWidness@users.noreply.github.com>
2021-02-09 12:20:34 +0300

    Hash password and verify

---
+++ EducationSystem/EducationSystem.Business/SecurityService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BCrypt.Net;
+
+namespace EducationSystem.Business
+{
+    class SecurityService
+    {
+
+        private const string _secret = "";
+
+        public string GetHash(string password)
+        { 
+             
+            return BCrypt.Net.BCrypt.HashPassword(password + _secret);
+        }
+
+        public bool VerifyHashAndPassword(string hashedPwdFromDatabase, string userEnteredPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(userEnteredPassword + _secret, hashedPwdFromDatabase);
+        }
+    }
+}
