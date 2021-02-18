fd6c602f049927f7573d51ffa99d562440956c61
Ira <brainforfreedom@gmail.com>
2021-02-19 02:12:25 +0300

    Create proc Comment_SelectByHomeworkId

---
+++ EducationSystem/EducationSystem.DB/StoredProcedures/Comment/dbo_Comment_SelectByHomeworkId.sql
@@ -0,0 +1,20 @@
+﻿CREATE proc dbo.Comment_SelectByHomeworkId (
+@homeworkId int)
+as
+begin
+	select
+		c.Id,
+		c.Message,
+		u.Id,
+		u.FirstName,
+		u.LastName,
+		u.Login,
+		u.UserPic,
+		hwa.Id
+	from dbo.Comment c
+		inner join dbo.[User] u on c.UserID = u.Id
+		inner join dbo.HomeworkAttempt hwa on c.HomeworkAttemptId = hwa.Id
+		inner join dbo.Homework hw on hwa.HomeworkId = hw.Id
+	where c.IsDeleted = 0 and hw.Id = @homeworkId
+end
+
