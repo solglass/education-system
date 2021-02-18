c4900a0b965211cdc0d06abeeeb8a9e292b06b59
Ira <brainforfreedom@gmail.com>
2021-02-19 00:02:49 +0300

    good name

---
+++ EducationSystem/EducationSystem.DB/StoredProcedures/Attachment/dbo.Attachment_SelectById.sql
@@ -0,0 +1,14 @@
+﻿CREATE proc [dbo].[Attachment_SelectById] (
+@id int
+) as begin
+	select 
+		a.Id, 
+		a.Path,
+		at.Id,
+		at.Name
+
+from [dbo].[Attachment] a
+	inner join [dbo].[AttachmentType] at 
+	on a.AttachmentTypeID = at.Id
+	where a.Id = @id
+end
