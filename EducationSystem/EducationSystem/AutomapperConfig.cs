9104d7b98ab74833286467175d90735fe79ea605
SimonShvets <goodweather.spb@gmail.com>
2021-02-19 22:00:54 +0300

    AutomapperAdd

---
+++ EducationSystem/EducationSystem/AutomapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using EducationSystem.API.Models.InputModels;
+using EducationSystem.API.Models.OutputModels;
+using EducationSystem.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationSystem.API
+{
+    public class AutomapperConfig : Profile
+    {
+        private const string _dateFormat = "dd.MM.yyyy";
+        public AutomapperConfig()
+        {
+            CreateMap<UserInputModel, UserDto>();
+            CreateMap<UserDto, UserOutputModel>()
+                .ForMember(dest => dest.BirthDate, opts => opts.MapFrom(src => src.BirthDate.ToString(_dateFormat)));
+        }
+    }
+}
--- EducationSystem/EducationSystem/Startup.cs
+++ EducationSystem/EducationSystem/Startup.cs
@@ -38,6 +38,7 @@ namespace EducationSystem
                         };
                     });
             services.AddControllers();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
