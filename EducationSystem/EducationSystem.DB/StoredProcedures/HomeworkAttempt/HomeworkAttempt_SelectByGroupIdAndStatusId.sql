7ee8d466875cbae764387119574eea6540e311f7
Frendor <frendor.dm@gmail.com>
2021-02-19 16:40:33 +0300

    Add HomeworkAttempt_SelectByUserId And SelectByGroupIdAndStatusId

--- EducationSystem/EducationSystem.DB/StoredProcedures/HomeworkAttempt/HomeworkAttempt_SelectByGroupIdAndStatusId.sql
+++ EducationSystem/EducationSystem.DB/StoredProcedures/HomeworkAttempt/HomeworkAttempt_SelectByGroupIdAndStatusId.sql
@@ -1,4 +1,6 @@
-﻿CREATE proc [dbo].[HomeworkAttempt_SelectAll]
+﻿CREATE proc [dbo].[HomeworkAttempt_SelectByGroupIdAndStatusId]
+(@Statusid int,
+@GroupId int)
 as
 begin
 	select
@@ -6,21 +8,26 @@ begin
 		hwa.Comment,
 		hwa.HomeworkId,
 		hwa.StatusId,
-		hwa.IsDeleted,
+		hwas.Id,
+		hwas.Name,
+		hw.Id,
+		hw.Description,
+		g.Id,
 		u.Id,
 		u.FirstName,
 		u.LastName,
 		u.UserPic,
-		hw.Description,
-		hwas.Name,
+		a.Id,
 		a.Path,
+		c.Id,
 		c.Message
 	from dbo.HomeworkAttempt hwa
 		inner join dbo.[User] u on hwa.UserId = u.Id
 		inner join dbo.Homework hw on hwa.HomeworkId = hw.Id
+		inner join dbo.[Group] g on hw.GroupID = g.Id 
 		inner join dbo.HomeworkAttemptStatus hwas on hwa.StatusId = hwas.Id
 		left join dbo.HomeworkAttempt_Attachment hwaa on hwaa.HomeworkAttemptID = hwa.Id
 		left join dbo.Attachment a on hwaa.AttachmentID = a.Id
 		left join dbo.Comment c on hwa.Id = c.HomeworkAttemptId
-	where hwa.IsDeleted = 0
+	where hwa.IsDeleted = 0 and g.Id = @GroupId and hwas.Id = @Statusid
 end
---
+++ EducationSystem/EducationSystem.DB/StoredProcedures/HomeworkAttempt/HomeworkAttempt_SelectByUserId.sql
@@ -0,0 +1,30 @@
+﻿CREATE proc [dbo].[HomeworkAttempt_SelectByUserId]
+(@id int)
+as
+begin
+	select
+		hwa.Id,
+		hwa.Comment,
+		hwa.HomeworkId,
+		hwa.StatusId,
+		hwas.Id,
+		hwas.Name,
+		hw.Id,
+		hw.Description,
+		u.Id,
+		u.FirstName,
+		u.LastName,
+		u.UserPic,
+		a.Id,
+		a.Path,
+		c.Id,
+		c.Message
+	from dbo.HomeworkAttempt hwa
+		inner join dbo.[User] u on hwa.UserId = u.Id
+		inner join dbo.Homework hw on hwa.HomeworkId = hw.Id
+		inner join dbo.HomeworkAttemptStatus hwas on hwa.StatusId = hwas.Id
+		left join dbo.HomeworkAttempt_Attachment hwaa on hwaa.HomeworkAttemptID = hwa.Id
+		left join dbo.Attachment a on hwaa.AttachmentID = a.Id
+		left join dbo.Comment c on hwa.Id = c.HomeworkAttemptId
+	where hwa.IsDeleted = 0 and u.id = @id
+end
