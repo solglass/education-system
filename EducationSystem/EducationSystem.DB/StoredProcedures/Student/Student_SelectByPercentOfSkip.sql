b6d8b61a48cd3ea6f8a1d1b04738e123c1ede4d5
Ira <brainforfreedom@gmail.com>
2021-02-24 18:15:57 +0300

    ESB-72 create proc Student_SelectByPercentOfSkip

---
+++ EducationSystem/EducationSystem.DB/StoredProcedures/Student/Student_SelectByPercentOfSkip.sql
@@ -0,0 +1,21 @@
+﻿CREATE proc [dbo].[Student_SelectByPercentOfSkip](
+@percent int
+)
+as
+begin
+	select
+		u.Id,
+		u.FirstName,
+		u.LastName,
+		u.Login,
+		p.PercentOfSkip
+	from
+		(
+		select
+			a.UserId,
+			cast (sum(case when a.IsAbsent=1 then 1.0 else 0 end)/ count(*)*100 as decimal(4,1)) as PercentOfSkip
+		from dbo.[Attendance] a 
+		group By a.UserId) as p
+	inner join dbo.[User] u on p.UserId=u.Id
+	where PercentOfSkip >= @percent
+end
