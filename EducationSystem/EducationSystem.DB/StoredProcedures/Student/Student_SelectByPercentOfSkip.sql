d36673460527cd5c9a784e684802164450d12b74
Ira <brainforfreedom@gmail.com>
2021-02-27 16:05:29 +0300

    ESB-72 fix proc Student_SelectByPercentOfSkip

--- EducationSystem/EducationSystem.DB/StoredProcedures/Student/Student_SelectByPercentOfSkip.sql
+++ EducationSystem/EducationSystem.DB/StoredProcedures/Student/Student_SelectByPercentOfSkip.sql
@@ -1,22 +1,28 @@
 ﻿CREATE proc [dbo].[Student_SelectByPercentOfSkip](
-@percent int
+@percent int,
+@groupId int
 )
 as
 begin
 	select
+		
 		u.Id,
 		u.FirstName,
 		u.LastName,
 		u.Login,
 		u.UserPic,
-		p.PercentOfSkip
+		p.PercentOfSkipLessons
+		
 	from
-		(
-		select
-			a.UserId,
-			cast (sum(case when a.IsAbsent=1 then 1.0 else 0 end)/ count(*)*100 as decimal(4,1)) as PercentOfSkip
-		from dbo.[Attendance] a 
-		group By a.UserId) as p
-	inner join dbo.[User] u on p.UserId=u.Id
-	where PercentOfSkip >= @percent
+			(
+			select
+				a.UserId,
+				cast (sum(case when a.IsAbsent=1 then 1.0 else 0 end)/ count(*)*100 as decimal(4,1)) as PercentOfSkipLessons
+			from dbo.[Attendance] a 
+			group By a.UserId
+			) as p
+		inner join dbo.[User] u on p.UserId=u.Id
+		inner join dbo.[Student_Group] sg on u.Id=sg.UserId
+		inner join dbo.[Group] g on sg.GroupId = g.Id
+	where PercentOfSkipLessons >= @percent and u.IsDeleted = 0 and g.StatusId = 3 and g.Id = @groupId
 end
