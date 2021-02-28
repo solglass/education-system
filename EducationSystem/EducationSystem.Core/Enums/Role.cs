dd37003b1609f56783d9d76217f4ef8a0051f939
SimonShvets <goodweather.spb@gmail.com>
2021-02-28 17:16:04 +0300

    Role enum added

---
+++ EducationSystem/EducationSystem.Core/Enums/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationSystem.Core.Enums
+{
+    public enum HomeworkAttemptStatus
+    {
+        ToDo = 1,
+        Expired,
+        ToCheck,
+        ToFix,
+        Passed,
+        PassedLate
+    }
+}
