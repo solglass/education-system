1a8fd25fd45e0fff3ca5c2f4416534207fce5bc3
Ira <brainforfreedom@gmail.com>
2021-03-05 15:57:29 +0300

    ESB-111 fix proc Student_SelectByPeriodAndIsNotPai

--- EducationSystem/EducationSystem.DB/StoredProcedures/Student/Student_SelectByPeriodAndIsNotPaid.sql
+++ EducationSystem/EducationSystem.DB/StoredProcedures/Student/Student_SelectByPeriodAndIsNotPaid.sql
@@ -1,13 +1,19 @@
 ﻿CREATE proc [dbo].[Student_SelectByPeriodAndIsNotPaid] (@period nvarchar(7))
 as
 begin
-	select DISTINCT
+	select
 		u.FirstName,
 		u.LastName,
-		u.Email,
-		u.Phone
-	from dbo.[Payment] p 
-		inner join dbo.[Student_Group] sg on p.ContractNumber=sg.ContractNumber
+		u.Phone,
+		u.Email
+	from
+			(select
+				p.ContractNumber,
+				sum (cast (p.IsPaid as int)) as isPaid
+			from dbo.[Payment] p 
+			where p.Period= @period
+			group by p.ContractNumber) as t
+		inner join dbo.[Student_Group] sg on t.ContractNumber=sg.ContractNumber
 		inner join dbo.[User] u on sg.UserID=u.Id
-	where p.IsPaid=0 and p.Period = @period
+	where t.isPaid=0
 end
