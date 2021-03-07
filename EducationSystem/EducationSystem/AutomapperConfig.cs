78fbc0e4db32a817aacbb188a40328e62f477436
VitaxaOv <vit2013aly@mail.ru>
2021-03-07 12:13:34 +0300

    ESB-141 Fix MapperConfig change Map for User

--- EducationSystem/EducationSystem/AutomapperConfig.cs
+++ EducationSystem/EducationSystem/AutomapperConfig.cs
@@ -7,6 +7,7 @@ using EducationSystem.Core.Enums;
 using EducationSystem.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,7 +19,7 @@ namespace EducationSystem.API
         public AutomapperConfig()
         {
             CreateMap<UserInputModel, UserDto>()
-                .ForMember(dest => dest.BirthDate, opts => opts.MapFrom(src => DateTime.Parse(src.BirthDate)))
+                .ForMember(dest => dest.BirthDate, opts => opts.MapFrom(src => DateTime.ParseExact(src.BirthDate, _dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None)))
                .ForMember(dest => dest.Roles, opts => opts.MapFrom(src => src.RoleIds.ConvertAll<Enum>(c=>(Role)c)));
             CreateMap<UserDto, AuthorOutputModel>();
             CreateMap<UserDto, UserOutputModel>()
