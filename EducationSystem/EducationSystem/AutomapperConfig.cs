dc7b284e0478ef9bd93b815edb8aece3c315d1b6
AleksandrM178 <myshalow_aleksandar@mail.ru>
2021-03-10 19:00:20 +0300

    Fixed autoMapper for enums

--- EducationSystem/EducationSystem/AutomapperConfig.cs
+++ EducationSystem/EducationSystem/AutomapperConfig.cs
@@ -100,7 +100,8 @@ namespace EducationSystem.API
                 .ForMember(dest => dest.HomeworkAttemptStatus, opts => opts.MapFrom(src => FriendlyNames.GetFriendlyHomeworkAttemptStatusName(src.HomeworkAttemptStatus)));
             CreateMap<AttachmentInputModel, AttachmentDto>()
                 .ForMember(dest => dest.AttachmentType, opts => opts.MapFrom(src => (AttachmentType)src.AttachmentTypeId));
-            CreateMap<AttachmentDto, AttachmentOutputModel>();
+            CreateMap<AttachmentDto, AttachmentOutputModel>()
+                .ForMember(dest => dest.AttachmentType, opts => opts.MapFrom(src => FriendlyNames.GetFriendlyAttachmentTypeName(src.AttachmentType)));
 
 
         }
