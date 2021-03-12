bbedb7f5bde551633fa1b16b6e8f4f0fdcaafcf5
SimonShvets <goodweather.spb@gmail.com>
2021-03-12 14:09:43 +0300

    proc [dbo].[PassedStudentsAttempt_SelectByGroupId] edded

---
+++ EducationSystem/EducationSystem.DB/StoredProcedures/User/dbo.PassedStudentsAttempt_SelectByGroupId.sql
@@ -0,0 +1,29 @@
+﻿create proc [dbo].[PassedStudentsAttempt_SelectByGroupId]
+(@groupId int) as
+begin
+ select
+    u.Id,
+    u.FirstName,
+    u.LastName,
+    u.[Login],
+	r.Id,
+	r.[Name]
+	
+    from dbo.HomeworkAttempt ha
+    inner join [User] u on ha.UserId = u.Id
+    inner join Student_Group sg on u.id = sg.UserID
+    inner join [Group] g on sg.GroupID = g.Id
+	inner join User_Role ur on u.Id = ur.UserID
+	inner join [Role] r on ur.RoleID = r.Id
+    where g.Id = @groupId and sg.UserId not in
+	(
+		select
+		sg.UserId
+
+		from dbo.HomeworkAttempt ha
+		inner join [User] u on ha.UserId = u.Id
+		inner join Student_Group sg on u.id = sg.UserID
+		inner join [Group] g on sg.GroupID = g.Id
+		where g.Id = @groupId and ha.StatusId !=3
+	) 
+end
