94566e0d001d388b0f72cafbd6b001cfceb46e2c
Ira <brainforfreedom@gmail.com>
2021-03-12 21:28:56 +0300

    ESB-214 Update MockMaterial

--- EducationSystem/EducationSystem.Test/MaterialMock.cs
+++ EducationSystem/EducationSystem.Test/MaterialMock.cs
@@ -10,8 +10,27 @@ namespace EducationSystem.Data.Tests
             {
                 Description = $"Description {caseId}",
                 Link = $"Link {caseId}",
-                IsDeleted = false
+                IsDeleted = CheckOddNumber(caseId)
             };
+        }
+
+        public static bool CheckOddNumber(int caseId)
+        {
+            if (caseId % 2 == 0)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
+
+/* for caseId 1-5
+ * Description, Link, IsDeleted
+ * Description1, Link1, 1
+ * Description2, Link2, 0
+ * Description3, Link3, 1
+ * Description4, Link4, 0
+ * Description5, Link5, 1
+ * 
+ */
