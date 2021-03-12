ba4f2da8acbd6a6aad6fa3af5729f93e1b9dbb9e
Ira <brainforfreedom@gmail.com>
2021-03-12 18:30:02 +0300

    ESB-214 Add MaterialOneTimeTearDown

--- EducationSystem/EducationSystem.Test/MaterialTests.cs
+++ EducationSystem/EducationSystem.Test/MaterialTests.cs
@@ -8,21 +8,28 @@ namespace EducationSystem.Data.Tests
     public class MaterialTests : BaseTest
     {
         private MaterialRepository _materialRepository;
-        
+        private List<int> _addedMaterialMockIds;
 
         [OneTimeSetUp]
         public void MaterialOneTimeSetUp()
         {
             _materialRepository = new MaterialRepository(_options);
+            _addedMaterialMockIds = new List<int>();
         }
 
         [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(4)]
         public void MaterialAddPositiveTest(int caseId)
         {
             var dto = MaterialMock.GetMaterialMock(caseId);
-            int id = _materialRepository.AddMaterial(dto);
-            dto.Id = id;
-            var actual = _materialRepository.GetMaterialById(id);
+            int addedId = _materialRepository.AddMaterial(dto);
+            dto.Id = addedId;
+            _addedMaterialMockIds.Add(addedId);
+
+            var actual = _materialRepository.GetMaterialById(addedId);
+
             Assert.AreEqual(dto, actual);
         }
 
@@ -30,16 +37,26 @@ namespace EducationSystem.Data.Tests
         public void MaterialUpdatePositiveTest(int caseId)
         {
             var dto = MaterialMock.GetMaterialMock(caseId);
-            int id = _materialRepository.AddMaterial(dto);
-            dto.Id = id;
+            int addedId = _materialRepository.AddMaterial(dto);
+            dto.Id = addedId;
+            _addedMaterialMockIds.Add(addedId);
 
             dto.Description = $"Description { caseId} update";
 
             var affectedRows = _materialRepository.UpdateMaterial(dto);
-            var actual = _materialRepository.GetMaterialById(id);
+            var actual = _materialRepository.GetMaterialById(addedId);
 
             Assert.AreEqual(1, affectedRows);
             Assert.AreEqual(dto, actual);
+        }
+
+        [OneTimeTearDown]
+        public void MaterialOneTimeTearDown()
+        {
+            _addedMaterialMockIds.ForEach(id =>
+            {
+                _materialRepository.HardDeleteMaterial(id);
+            });
         }
     }
 }
