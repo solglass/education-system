a4c778c6f660290ed0827d71f3d235d562ee0454
AleksandrM178 <myshalow_aleksandar@mail.ru>
2021-03-12 22:51:27 +0300

    Add Equals to lesson and create LessonTests class

--- EducationSystem/EducationSystem.Data/Models/LessonDto.cs
+++ EducationSystem/EducationSystem.Data/Models/LessonDto.cs
@@ -5,12 +5,23 @@ using System.Text;
 namespace EducationSystem.Data.Models
 {
    public  class LessonDto
-    {
+   {
         public int Id { get; set; }
         public GroupDto Group { get; set; }
         public string Comment { get; set; }
         public DateTime Date { get; set; }
         public bool IsDeleted { get; set; }
         public List<ThemeDto> Themes { get; set; }
-    }
+
+
+        public override bool Equals(object obj)
+        {
+            LessonDto lesson = (LessonDto)obj;
+            if(!Id.Equals(lesson.Id) || !Comment.Equals(lesson.Comment) || !Date.Equals(lesson.Date))
+            {
+                return false;
+            }
+            return true;
+        }
+   }
 }
