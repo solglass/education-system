a5b344d6164cfc107ab9acb0d0e3a4890ddaf358
Ira <brainforfreedom@gmail.com>
2021-03-12 17:20:51 +0300

    ESB-214 Add MaterialAddPositiveTest and fix dapper in MaterialRepository

--- EducationSystem/EducationSystem.Test/MaterialTests.cs
+++ EducationSystem/EducationSystem.Test/MaterialTests.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EducationSystem.Data.Tests
 {
-    class MaterialTests
+    public class MaterialTests : BaseTest
     {
+        private MaterialRepository _materialRepository;
+        
+
+        [OneTimeSetUp]
+        public void MaterialOneTimeSetUp()
+        {
+            _materialRepository = new MaterialRepository(_options);
+        }
+
+        [TestCase(1)]
+        public void MaterialAddPositiveTest(int caseId)
+        {
+            var dto = MaterialMock.GetMaterialMock(caseId);
+            int id = _materialRepository.AddMaterial(dto);
+            dto.Id = id;
+            var actual = _materialRepository.GetMaterialById(id);
+            Assert.AreEqual(dto, actual);
+        }
     }
 }
