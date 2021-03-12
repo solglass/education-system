e7376b1af0769cfaac0be6f34233a9f4e3c9ef0f
Ira <brainforfreedom@gmail.com>
2021-03-12 16:23:04 +0300

    ESB-214 Add MaterialMock, add MaterialTests

---
+++ EducationSystem/EducationSystem.Test/MaterialMock.cs
@@ -0,0 +1,17 @@
+﻿using EducationSystem.Data.Models;
+
+namespace EducationSystem.Data.Tests
+{
+    public static class MaterialMock
+    {
+        public static MaterialDto GetMaterialMock(int caseId)
+        {
+            return new MaterialDto
+            {
+                Description = $"Description {caseId}",
+                Link = $"Link {caseId}",
+                IsDeleted = false
+            };
+        }
+    }
+}
