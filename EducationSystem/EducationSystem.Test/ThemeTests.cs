d5c110867655fbd18b32c789464fe5d0685cdc2c
Oleg Lyamzin <o.s.like@yandex.ru>
2021-03-15 15:44:43 +0300

    ESB-187: GetUncoveredThemesByGroupIdPositiveTest

--- EducationSystem/EducationSystem.Test/ThemeTests.cs
+++ EducationSystem/EducationSystem.Test/ThemeTests.cs
@@ -11,18 +11,28 @@ namespace EducationSystem.Data.Tests
     {
 
         private ICourseRepository _courseRepo;
+        private IGroupRepository _groupRepo;
+        private ILessonRepository _lessonRepo;
         private List<int> _themeIdList;
         private List<int> _courseIdList;
+        private List<int> _groupIdList;
+        private List<int> _lessonIdList;
         private List<(int, int)> _courseThemeList;
+        private List<(int, int)> _lessonThemeList;
 
         [OneTimeSetUp]
         public void SetUpTest()
         {
             _courseRepo = new CourseRepository(_options);
+            _groupRepo = new GroupRepository(_options);
+            _lessonRepo = new LessonRepository(_options);
 
             _themeIdList = new List<int>();
             _courseIdList = new List<int>();
+            _groupIdList = new List<int>();
+            _lessonIdList = new List<int>();
             _courseThemeList = new List<(int, int)>();
+            _lessonThemeList = new List<(int, int)>();
         }
 
         [TestCase(1)]
@@ -145,12 +155,85 @@ namespace EducationSystem.Data.Tests
             CollectionAssert.AreEqual(expected, actual);
         }
 
+        [TestCase(new int[] { 1, 2, 3 })]
+        [TestCase(new int[] { 3, 2, 1 })]
+        [TestCase(new int[] { 1, 2, 3, 2, 1, 3, 2, 1 })]
+        public void GetUncoveredThemesByGroupIdPositiveTest(int[] mockIds)
+        {
+            // Given
+            var courseDto = (CourseDto)CourseMockGetter.GetCourseDtoMock(1);
+            var addedCourseId = _courseRepo.AddCourse(courseDto);
+            _courseIdList.Add(addedCourseId);
+            courseDto.Id = addedCourseId;
+
+            var groupDto = (GroupDto)GroupMockGetter.GetGroupDtoMock(1);
+            groupDto.Course = courseDto;
+            var addedGroupId = _groupRepo.AddGroup(groupDto);
+            _groupIdList.Add(addedGroupId);
+            groupDto.Id = addedGroupId;
+
+            var lessonDto = (LessonDto)LessonMockGetter.GetLessonDtoMock(1).Clone();
+            lessonDto.Date = DateTime.Now.AddDays(10);
+            lessonDto.Group = groupDto;
+            var addedLessonId = _lessonRepo.AddLesson(lessonDto);
+            _lessonIdList.Add(addedLessonId);
+
+            var expected = new List<ThemeDto>();
+            for (int i = 0; i < mockIds.Length; i++)
+            {
+                var dto = (ThemeDto)ThemeMockGetter.GetThemeDtoMock(mockIds[i]).Clone();
+                var addedThemeId = _courseRepo.AddTheme(dto);
+                _themeIdList.Add(addedThemeId);
+                dto.Id = addedThemeId;
+                expected.Add(dto);
+
+                _courseRepo.AddCourse_Theme(addedCourseId, addedThemeId);
+                _courseThemeList.Add((addedCourseId, addedThemeId));
+
+                _lessonRepo.AddLessonTheme(addedLessonId, addedThemeId);
+                _lessonThemeList.Add((addedLessonId, addedThemeId));
+            }
+
+            // When
+            var actual = _courseRepo.GetUncoveredThemesByGroupId(addedGroupId);
+
+            // Then
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
         [OneTimeTearDown]
         public void SampleTestTearDown()
         {
             DeleteCourseThemes();
+            DeleteLessonThemes();
+            DeleteLessons();
+            DeleteGroups();
             DeleteCourses();
             DeleteThemes();
+        }
+
+        private void DeleteLessons()
+        {
+            foreach (var lessonId in _lessonIdList)
+            {
+                _lessonRepo.HardDeleteLesson(lessonId);
+            }
+        }
+
+        private void DeleteLessonThemes()
+        {
+            foreach (var lessonTheme in _lessonThemeList)
+            {
+                _lessonRepo.DeleteLessonTheme(lessonTheme.Item1, lessonTheme.Item2);
+            }
+        }
+
+        private void DeleteGroups()
+        {
+            foreach (var groupId in _groupIdList)
+            {
+                _groupRepo.HardDeleteGroup(groupId);
+            }
         }
 
         private void DeleteCourseThemes()
