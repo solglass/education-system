54d3d712123207352e787dc909b6c78b8600bb3f
CosmoStas <stasbkj@gmail.com>
2021-03-16 20:12:27 +0300

    Add method's Clone and Equals

--- EducationSystem/EducationSystem.Test/Mocks/FeedbackMockGetter.cs
+++ EducationSystem/EducationSystem.Test/Mocks/FeedbackMockGetter.cs
@@ -17,21 +17,18 @@ namespace EducationSystem.Data.Tests.Mocks
                         Message = "Test 1 mock",
                         UnderstandingLevel = Core.Enums.UnderstandingLevel.Bad
                     };
-                    break;
                 case 2:
                     return new FeedbackDto
                     {
                         Message = "Test 2 mock",
                         UnderstandingLevel = Core.Enums.UnderstandingLevel.Good
                     };
-                    break;
                 case 3:
                     return new FeedbackDto
                     {
                         Message = "Test 3 mock",
                         UnderstandingLevel = Core.Enums.UnderstandingLevel.Medium
                     };
-                    break;
                 default:
                     return null;
             }
