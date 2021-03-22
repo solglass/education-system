3df987afa9b785c72d4b5b2d3fcad78cdecffe62
Ira <brainforfreedom@gmail.com>
2021-03-22 18:42:25 +0300

    ESB-196 SetUp and TearDown for Group

---
+++ EducationSystem/EducationSystem.Test/GroupTests.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationSystem.Data.Tests
+{
+    public class GroupTests : BaseTest
+    {
+        private IGroupRepository _groupRepository;
+        private ICourseRepository _courseRepository;
+        private IMaterialRepository _materialRepository;
+
+        private List<int> _addedMaterialMockIds;
+        private List<int> _addedGroupIds;
+        private List<int> _addedCourseIds;
+        private List<(int, int)> _addedMaterialGroupIds;
+
+        [OneTimeSetUp]
+        public void GroupOneTimeSetUp()
+        {
+            _materialRepository = new MaterialRepository(_options);
+            _groupRepository = new GroupRepository(_options);
+            _courseRepository = new CourseRepository(_options);
+
+            _addedMaterialMockIds = new List<int>();
+            _addedGroupIds = new List<int>();
+            _addedCourseIds = new List<int>();
+            _addedMaterialGroupIds = new List<(int, int)>();
+        }
+
+        [OneTimeTearDown]
+        public void GroupOneTimeTearDown()
+        {
+            DeleteMaterialGroups();
+            DeleteMaterials();
+            DeleteGroups();
+            DeleteCourses();
+        }
+
+        public void DeleteMaterials()
+        {
+            _addedMaterialMockIds.ForEach(id =>
+            {
+                _materialRepository.HardDeleteMaterial(id);
+            });
+        }
+        public void DeleteGroups()
+        {
+            _addedGroupIds.ForEach(id =>
+            {
+                _groupRepository.HardDeleteGroup(id);
+            });
+        }
+        public void DeleteMaterialGroups()
+        {
+            _addedMaterialGroupIds.ForEach(id =>
+            {
+                _groupRepository.DeleteGroup_Material(id.Item1, id.Item2);
+            });
+        }
+        public void DeleteCourses()
+        {
+            _addedCourseIds.ForEach(id =>
+            {
+                _courseRepository.HardDeleteCourse(id);
+            });
+        }
+    }
+}
