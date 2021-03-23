8db91d72e6e08b7bb080bef6e294c6f039545162
Ira <brainforfreedom@gmail.com>
2021-03-23 21:47:30 +0300

    ESB-227 Validation for methods of MaterialController

--- EducationSystem/EducationSystem/Controllers/MaterialController.cs
+++ EducationSystem/EducationSystem/Controllers/MaterialController.cs
@@ -1,19 +1,13 @@
 ﻿using AutoMapper;
-using EducationSystem.API.Mappers;
 using EducationSystem.API.Models.InputModels;
 using EducationSystem.API.Models.OutputModels;
 using EducationSystem.Business;
-using EducationSystem.Controllers;
-using EducationSystem.Data;
+using EducationSystem.Core.CustomExceptions;
 using EducationSystem.Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EducationSystem.Controllers
 {
@@ -42,7 +36,6 @@ namespace EducationSystem.Controllers
         public ActionResult<List<MaterialOutputModel>> GetMaterials()
         {
             return Ok(_mapper.Map<List<MaterialOutputModel>>(_service.GetMaterials()));
-
         }
 
         // https://localhost:44365/api/material/by-group/340
@@ -50,11 +43,15 @@ namespace EducationSystem.Controllers
         /// <param name="id">Id of group, which materials is needed</param>
         /// <returns>List of attached materials to group</returns>
         [ProducesResponseType(typeof(List<MaterialOutputModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("by-group/{id}")]
         [Authorize(Roles = "Admin, Teacher, Tutor, Methodist, Student")]
         public ActionResult<List<MaterialOutputModel>> GetMaterialsByGroupId(int id)
         {
-            return Ok(_mapper.Map<List<MaterialOutputModel>>(_service.GetMaterialsByGroupId(id)));
+            var dtos = _service.GetMaterialsByGroupId(id);
+            if (dtos is null)
+                return NotFound($"Group {id} not found");
+            return Ok(_mapper.Map<List<MaterialOutputModel>>(dtos));
 
         }
 
@@ -63,11 +60,15 @@ namespace EducationSystem.Controllers
         /// <param name="id">Id of tag, which materials is needed</param>
         /// <returns>List of attached materials to tag</returns>
         [ProducesResponseType(typeof(List<MaterialOutputModel>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("by-tag/{id}")]
         [Authorize(Roles = "Admin, Teacher, Tutor, Methodist, Student")]
         public ActionResult<List<MaterialOutputModel>> GetMaterialsByTagId(int id)
         {
-            return Ok(_mapper.Map<List<MaterialOutputModel>>(_service.GetMaterialsByTagId(id)));
+            var dtos = _service.GetMaterialsByTagId(id);
+            if (dtos is null)
+                return NotFound($"Tag {id} not found");
+            return Ok(_mapper.Map<List<MaterialOutputModel>>(dtos));
         }
 
         // https://localhost:44365/api/material/2
@@ -75,11 +76,15 @@ namespace EducationSystem.Controllers
         /// <param name="id">Id of material</param>
         /// <returns>Material</returns>
         [ProducesResponseType(typeof(MaterialOutputModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{id}")]
         [Authorize(Roles = "Admin, Teacher, Tutor, Methodist, Student")]
         public ActionResult<MaterialOutputModel> GetMaterialById(int id)
         {
-            return Ok(_mapper.Map<MaterialOutputModel>(_service.GetMaterialById(id)));
+            var dtos = _service.GetMaterialById(id);
+            if (dtos is null)
+                return NotFound($"Material {id} not found");
+            return Ok(_mapper.Map<MaterialOutputModel>(dtos));
         }
 
         // https://localhost:44365/api/material
@@ -87,10 +92,13 @@ namespace EducationSystem.Controllers
         /// <param name="materialInputModel">Info about added material (Source link, Description of content)</param>
         /// <returns>Added material</returns>
         [ProducesResponseType(typeof(MaterialOutputModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         [HttpPost]
         [Authorize(Roles = "Admin, Teacher, Tutor, Methodist")]
         public ActionResult<MaterialOutputModel> AddNewMaterial([FromBody] MaterialInputModel materialInputModel)
         {
+            if (!ModelState.IsValid)
+                throw new ValidationException(ModelState);
             var id = _service.AddMaterial(_mapper.Map<MaterialDto>(materialInputModel));
             return Ok(_mapper.Map<MaterialOutputModel>(_service.GetMaterialById(id)));
         }
@@ -101,10 +109,16 @@ namespace EducationSystem.Controllers
         /// <param name="material">All info about update material</param>
         /// <returns>Update material</returns>
         [ProducesResponseType(typeof(MaterialOutputModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
         [HttpPut("{id}")]
         [Authorize(Roles = "Admin, Teacher, Tutor, Methodist")]
         public ActionResult<MaterialOutputModel> UpdateMaterial(int id, [FromBody] MaterialInputModel material)
         {
+            if (_service.GetMaterialById(id) is null)
+                return NotFound($"Material {id} not found");
+            if (!ModelState.IsValid)
+                throw new ValidationException(ModelState);
             var dto = _mapper.Map<MaterialDto>(material);
             dto.Id = id;
             _service.UpdateMaterial(dto);
@@ -117,10 +131,13 @@ namespace EducationSystem.Controllers
         /// <param name="id">Id of material</param>
         /// <returns>Update material, which is deleted</returns>
         [ProducesResponseType(typeof(MaterialOutputModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpDelete("{id}")]
         [Authorize(Roles = "Admin, Teacher, Tutor")]
         public ActionResult<MaterialOutputModel> DeleteMaterial(int id)
         {
+            if (_service.GetMaterialById(id) is null)
+                return NotFound($"Material {id} not found");
              _service.DeleteMaterial(id);
             var result = _mapper.Map<MaterialOutputModel>(_service.GetMaterialById(id));
             return Ok(result);
@@ -131,10 +148,13 @@ namespace EducationSystem.Controllers
         /// <param name="id">Id of material</param>
         /// <returns>Update material, which is recovered</returns>
         [ProducesResponseType(typeof(MaterialOutputModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpPut("{id}/recovery")]
         [Authorize(Roles = "Admin, Teacher, Tutor")]
         public ActionResult<MaterialOutputModel> RecoverMaterial(int id)
         {
+            if (_service.GetMaterialById(id) is null)
+                return NotFound($"Material {id} not found");
             _service.RecoverMaterial(id);
             var result = _mapper.Map<MaterialOutputModel>(_service.GetMaterialById(id));
             return Ok(result);
