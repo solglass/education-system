bcba395092b387836b25737b2c86fc31f82ebb02
Aleksandr <myshalow_aleksandar@mail.ru>
2021-03-31 18:19:39 +0300

    Add update user input model

---
+++ EducationSystem/EducationSystem/Models/InputModels/UpdateUserInputModel.cs
@@ -0,0 +1,36 @@
+﻿using EducationSystem.API.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EducationSystem.API.Models.InputModels
+{
+    public class UpdateUserInputModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+        [CustomDateTimeValidation]
+        [Required]
+        public string BirthDate { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Login { get; set; }            
+        [Required]
+        [StringLength(20)]
+        public string Phone { get; set; }
+        [Required]
+        [StringLength(1000)]
+        public string UserPic { get; set; }
+        [Required]
+        [EmailAddress]
+        [StringLength(60)]
+        public string Email { get; set; }
+        public List<int> RoleIds { get; set; }
+    }
+}
