24bfe102160da42a7b5cfdb642cd00dea6bdd5db
Denis Valeev <valeevden@gmail.com>
2021-03-31 03:28:43 +0300

    ESB-219 Strart Notification test
    MockGetter and notification table fix

---
+++ EducationSystem/EducationSystem.Test/Mocks/NotificationMockGetter.cs
@@ -0,0 +1,41 @@
+﻿using EducationSystem.Data.Models;
+using System;
+
+namespace EducationSystem.Data.Tests.Mocks
+{
+    public static class NotificationMockGetter
+    {
+        public static NotificationDto GetNotificationDtoMock(int id)
+        {
+            return id switch
+            {
+                1 => new NotificationDto()
+                {
+                    User = new UserDto(),
+                    Author = new UserDto(),
+                    Message = "Тобi Жопа",
+                    Date = new DateTime(2021, 03, 30, 11, 30, 0).AddDays(-1),
+                    IsRead = false
+                },
+                2 => new NotificationDto()
+                {
+                    User = new UserDto(),
+                    Author = new UserDto(),
+                    Message = "Тобi НЕ жопа",
+                    Date = new DateTime(2021, 03, 30, 11, 30, 0).AddDays(-2),
+                    IsRead = false
+                },
+                3 => new NotificationDto()
+                {
+                    User = new UserDto(),
+                    Author = new UserDto(),
+                    Message = "Текст сообщения",
+                    Date = new DateTime(2021, 03, 30, 11, 30, 0).AddDays(+3),
+                    IsRead = false
+                },
+                4 => new NotificationDto(),
+                _ => null,
+            };
+        }
+    }
+}
---
+++ EducationSystem/EducationSystem.Test/NotificationTests.cs
@@ -0,0 +1,172 @@
+﻿using EducationSystem.Data.Models;
+using EducationSystem.Data.Tests.Mocks;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace EducationSystem.Data.Tests
+{
+    public class NotificationTests : BaseTest
+    {
+        private IHomeworkRepository _homeworkRepo;
+        private IUserRepository _userRepo;
+        private IGroupRepository _groupRepo;
+        private ICourseRepository _courseRepo;
+        private IAttachmentRepository _attachmentRepo;
+        private INotificationRepository _notificationRepo;
+
+        private UserDto _userDtoMock;
+        private UserDto _authorDtoMock;
+        private GroupDto _groupDtoMock;
+        private HomeworkDto _homeworkDtoMock;
+
+        private List<int> _homeworkAttemptIdList; 
+        private List<int> _homeworkIdList; 
+        private List<int> _userIdList;
+        private List<int> _groupIdList;
+        private List<int> _courseIdList;
+        private List<int> _attachmentIdList;
+        private List<(int, int)> _attemptAttachmentIdList;
+        private List<int> _notificationIdList; 
+
+        [SetUp]
+        public void SetUpTest()
+        {
+            _homeworkRepo = new HomeworkRepository(_options);
+            _userRepo = new UserRepository(_options);
+            _groupRepo = new GroupRepository(_options);
+            _courseRepo = new CourseRepository(_options);
+            _attachmentRepo = new AttachmentRepository(_options);
+            _notificationRepo = new NotificationRepository(_options);
+
+            _homeworkAttemptIdList = new List<int>();
+            _homeworkIdList = new List<int>();
+            _userIdList = new List<int>();
+            _groupIdList = new List<int>();
+            _courseIdList = new List<int>();
+            _attachmentIdList = new List<int>();
+            _attemptAttachmentIdList = new List<(int, int)>();
+            _notificationIdList = new List<int>();
+
+            _userDtoMock = (UserDto)UserMockGetter.GetUserDtoMock(1).Clone();
+            _userDtoMock.Id = _userRepo.AddUser(_userDtoMock);
+            _userIdList.Add(_userDtoMock.Id);
+
+            _authorDtoMock = (UserDto)UserMockGetter.GetUserDtoMock(2).Clone();
+            _authorDtoMock.Id = _userRepo.AddUser(_authorDtoMock);
+            _userIdList.Add(_authorDtoMock.Id);
+
+            _groupDtoMock = (GroupDto)GroupMockGetter.GetGroupDtoMock(1).Clone();
+            _groupDtoMock.Course = (CourseDto)CourseMockGetter.GetCourseDtoMock(1).Clone();
+            var addedCourseId = _courseRepo.AddCourse(_groupDtoMock.Course);
+            _courseIdList.Add(addedCourseId);
+            _groupDtoMock.Course.Id = addedCourseId;
+            var addedGroupId = _groupRepo.AddGroup(_groupDtoMock);
+            _groupIdList.Add(addedGroupId);
+            _groupDtoMock.Id = addedGroupId;
+
+            _homeworkDtoMock = (HomeworkDto)HomeworkMockGetter.GetHomeworkDtoMock(1).Clone();
+            _homeworkDtoMock.Group = _groupDtoMock;
+            var addedhomeworkId = _homeworkRepo.AddHomework(_homeworkDtoMock);
+            _homeworkIdList.Add(addedhomeworkId);
+            _homeworkDtoMock.Id = addedhomeworkId;
+        }
+
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void AddNotificationPositiveTest(int mockId)
+        {
+            // Given
+            var dto = (NotificationDto)NotificationMockGetter.GetNotificationDtoMock(mockId).Clone();
+            dto.User = _userDtoMock;
+            dto.Author = _authorDtoMock;
+            var addedNotificationId = _notificationRepo.AddNotification(dto);
+            Assert.Greater(addedNotificationId, 0);
+
+            _notificationIdList.Add(addedNotificationId);
+            dto.Id = addedNotificationId;
+
+            // When
+            var actual = _notificationRepo.GetNotificationById(addedNotificationId);
+
+            // Then
+            Assert.AreEqual(dto, actual);
+        }
+
+        
+
+        [TearDown]
+        public void TestTearDown()
+        {
+            DeleteNotifications();
+            DeleteHomworkAttemptAttachments();
+            DeleteAttachments();
+            DeleteHomeworkAttempt();
+            DeleteHomework();
+            DeleteGroups();
+            DeleteCourse();
+            DeleteUser();
+        }
+        private void DeleteNotifications()
+        {
+            foreach (var item in _notificationIdList)
+            {
+                _notificationRepo.DeleteNotification(item);
+            }
+        }
+        private void DeleteHomworkAttemptAttachments()
+        {
+            foreach (var item in _attemptAttachmentIdList)
+            {
+                _attachmentRepo.DeleteAttachmentFromHomeworkAttempt(item.Item2, item.Item1);
+            }
+        }
+        private void DeleteAttachments()
+        {
+            foreach(var item in _attachmentIdList)
+            {
+                _attachmentRepo.DeleteAttachmentById(item);
+            }
+        }
+
+        private void DeleteUser()
+        {
+            foreach (var userId in _userIdList)
+            {
+                _userRepo.HardDeleteUser(userId);
+            }
+        }
+
+        private void DeleteCourse()
+        {
+            foreach (int courseId in _courseIdList)
+            {
+                _courseRepo.HardDeleteCourse(courseId);
+            }
+        }
+
+        private void DeleteGroups()
+        {
+            foreach (int groupId in _groupIdList)
+            {
+                _groupRepo.HardDeleteGroup(groupId);
+            }
+        }
+
+        private void DeleteHomework()
+        {
+            foreach (var homeworkId in _homeworkIdList)
+            {
+                _homeworkRepo.HardDeleteHomework(homeworkId);
+            }
+        }
+
+        private void DeleteHomeworkAttempt()
+        {
+            foreach (var hwAttemptId in _homeworkAttemptIdList)
+            {
+                _homeworkRepo.HardDeleteHomeworkAttempt(hwAttemptId);
+            }
+        }
+    }
+}
