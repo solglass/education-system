03395bf853e8a4ed67cb004c722a1bc663652c89
CosmoStas <stasbkj@gmail.com>
2021-05-07 19:47:36 +0300

    Fix Homework search

--- EducationSystem/EducationSystem.DB/StoredProcedures/Homework/dbo.Homework_Search.sql
+++ EducationSystem/EducationSystem.DB/StoredProcedures/Homework/dbo.Homework_Search.sql
@@ -1,5 +1,4 @@
 ﻿create PROCEDURE [dbo].[Homework_Search]
-@groupId int = null,
 @themeId int = null,
 @tagId int = null,
 @courseId int = null
@@ -11,7 +10,6 @@ SELECT h.[Id]
       ,h.[DeadlineDate]
       ,h.[IsOptional]
       ,c.Id
-      ,g.Id
       ,tg.Id
       ,tg.[Name]
       ,th.Id
@@ -21,13 +19,12 @@ SELECT h.[Id]
   left join dbo.Theme th on hth.ThemeId = th.id
 
   left join dbo.Homework_Tag htg on h.id = htg.HomeworkId
- left join dbo.Tag tg on htg.TagId = tg.id
+  left join dbo.Tag tg on htg.TagId = tg.id
 
   inner join dbo.[Course] c on h.CourseID = c.id
   
   where h.IsDeleted = 0 and 
   (@courseId is not null and c.Id = @courseId or @courseId is null) and
-  (@groupId is not null and g.Id = @groupId or @groupId is null) and
   (@themeId is not null and th.Id = @themeId or @themeId is null) and
   (@tagId is not null and tg.Id = @tagId or @tagId is null)
   
